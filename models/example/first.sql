38ac19374f7cf75b3f1a2022cff20a89ed2fe89e
shinely394@gmail.com <shinely394@gmail.com>
2022-07-04 11:47:48 +0000

    COMMIT

---
+++ models/example/first.sql
@@ -0,0 +1,2 @@
+{{ config(materialized='table') }}
+SELECT * FROM "VWE_S3"."PUBLIC"."MULTI_ATTRIBUTE"
--- models/first.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='table') }}
-
- SELECT * FROM" "VWE_S3"."PUBLIC"."multi_attribute"
